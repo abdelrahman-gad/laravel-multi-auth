e7046db02df630dc7b062fe275718f572d68e5e5
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-04-15 00:11:11 +0200

    adding flash sessions

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -47,7 +46,6 @@
                  
                 </div>
             </div>
-        </div>
-    </div>
+      </div>
 </div>
 @endsection
